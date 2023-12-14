9694c8d7948a26d9fbf86dcaebb80563cf1a4df6
Daniel Shulgin <danielshulgin37@gmail.com>
2023-12-14 12:52:55 +0100

    init

---
+++ src/main/java/com/petproject/taskmanagertelegramapi/TaskManagerTelegramApiApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.taskmanagertelegramapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerTelegramApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerTelegramApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petproject/taskmanagertelegramapi/TaskManagerTelegramApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.taskmanagertelegramapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerTelegramApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
